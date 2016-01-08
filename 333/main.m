5ec91e166068ffe7ed817866857c17ce805eed7d
管复生 <975081801@qq.com>
2016-01-08 23:08:28 +0800

    Initial Commit

---
+++ 333/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  333
+//
+//  Created by 管复生 on 16/1/8.
+//  Copyright © 2016年 GFS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
